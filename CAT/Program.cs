90d04e251cad4f27eda38c5443564d45c320e5f9
Михаил Кочергин <114687659+finedandelion@users.noreply.github.com>
2025-03-20 17:08:00 +0500

    Cors & ForwardedHeaders
    Подключены корсы, пока без ограничений.
    Подключены заголовки для nginx.

--- CAT/Program.cs
+++ CAT/Program.cs
@@ -1,10 +1,25 @@
+using Microsoft.AspNetCore.HttpOverrides;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddCors();
 
 var app = builder.Build();
+
+app.UseCors(builder =>
+     builder.AllowAnyOrigin()
+         .AllowAnyHeader()
+         .AllowAnyMethod()
+         .WithExposedHeaders("Content-Disposition")
+);
+
+app.UseForwardedHeaders(new ForwardedHeadersOptions
+{
+    ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto
+});
 
 if (app.Environment.IsDevelopment())
 {
