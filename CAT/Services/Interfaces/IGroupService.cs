c000c43fb6db9a1b861c2901a576559d88733f3d
mylongcode <mylongcode@gmail.com>
2025-04-10 23:14:11 +0300

    add methods for infrasctucture

--- CAT/Services/Interfaces/IGroupService.cs
+++ CAT/Services/Interfaces/IGroupService.cs
@@ -1,6 +1,20 @@
-﻿namespace CAT.Services.Interfaces
+﻿using CAT.Controllers.DTO;
+using CAT.EF.DAL;
+
+namespace CAT.Services.Interfaces
 {
-    public class IGroupService
+    public interface IGroupService
     {
+        public void RegistrationGroup(GroupRegistrationDTO dto);
+        public List<GroupTypeDTO> GetGroupTypes(Guid org_id);
+        public List<GroupInfrasctructureDTO> GetGroupsByOrganization(Guid org_id);
+        public bool CreateGroup(CreateGroupDTO dto, Guid org_id);
+        public bool CreateGroupType(CreateGroupTypeDTO dto, Guid org_id);
+        public List<IdentificationInfoDTO> GetIdentificationsByOrganization(Guid organizationId);
+        public bool CreateIdentification(CreateIdentificationDTO dto, Guid organizationId);
+        public bool DeleteGroupType(Guid typeId);
+        public bool DeleteGroup(Guid groupId);
+        public void DeleteIdentification(Guid groupId);
+        void EditGroup(EditGroupDTO dto, Guid organizationId);
     }
 }
