6aa838eb1aa3d72939fd63d9269dd65becb68a31
mylongcode <mylongcode@gmail.com>
2025-05-08 21:41:07 +0500

    delete insemination status

--- CAT/Controllers/ReproductiveController.cs
+++ CAT/Controllers/ReproductiveController.cs
@@ -64,7 +64,6 @@ namespace CAT.Controllers
             {
                 CowId = dto.CowId,
                 Date = dto.Date,
-                Status = dto.Status,
                 ExpectedCalvingDate = dto.ExpectedCalvingDate
             };
             _animalService.InsertPregnancy(pregnancy);
