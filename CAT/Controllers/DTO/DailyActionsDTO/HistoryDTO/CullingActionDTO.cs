74c148b1db125af0ef0f6ec95d4a31278798fb41
finedandelion <idr.minki@gmail.com>
2025-05-14 18:20:49 +0500

    Удалил ненужные ДТОшки

--- CAT/Controllers/DTO/DailyActionsDTO/HistoryDTO/CullingActionDTO.cs
+++
@@ -1,6 +0,0 @@
-using CAT.Controllers.DTO;
-
-public class CullingActionDTO : DailyActionDTO
-    {
-        public string? Type { get; init; }
-    }
--- CAT/Controllers/DTO/DailyActionsDTO/HistoryDTO/IdentificationActionDTO.cs
+++
@@ -1,6 +0,0 @@
-using System.ComponentModel.DataAnnotations;
-
-namespace CAT.Controllers.DTO
-{
-    public class IdentificationActionDTO : DailyActionDTO{}
-}
